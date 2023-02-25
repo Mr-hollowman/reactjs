392eef15ccb480250052f580971508c7c4c690c3
mr-hollowman <selvamani825@gmail.com>
2023-02-25 08:38:36 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-// import Kumar from './Task/kumar';
+import App from './sivabro/App';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-    
-      <App/>
-      {/* <Kumar/> */}
+    <App />
     </BrowserRouter>
 );
---
+++ src/sivabro/Slider.js
@@ -0,0 +1,10 @@
+export default function Slider() {
+  return (
+    <div className="slider">
+      <div className="brand-banner">
+        <h3>Joe's Kitchen</h3>
+        <p>Have a break Eat Fresh..!</p>
+      </div>
+    </div>
+  )
+}
